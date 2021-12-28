de590f868790d7b67b28050f55d80360e57dfa58
Kelvin <et.kelvin@gmail.com>
2021-12-28 22:54:48 +0800

    bug fix: 修复 favicon 不显示的问题

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -12,7 +12,7 @@ export default function Layout({ children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
         <meta name="description" content={PAGE_DESCRIPTION} />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="favicon.ico" />
         <title>{PAGE_TITLE}</title>
       </Head>
       <div className="container">
