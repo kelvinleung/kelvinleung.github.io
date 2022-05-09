1d3b94b21ed1a08439d81fbd67451b01c7700c50
Kelvin <et.kelvin@gmail.com>
2022-05-09 21:36:43 +0800

    优化小屏导航栏样式

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -2,9 +2,73 @@ import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
 
+const MenuButton = ({ onToggle }) => {
+  return (
+    <div className="navbar-menu-button" onClick={onToggle}>
+      <svg
+        viewBox="0 0 1024 1024"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M954.88 237.056H69.12a43.52 43.52 0 1 1 0-87.04h885.248c24.064 0 43.52 19.456 43.52 43.52s-18.944 43.52-43.008 43.52zM954.88 555.52H69.12a43.52 43.52 0 1 1 0-87.04h885.248c24.064 0 43.52 19.456 43.52 43.52s-18.944 43.52-43.008 43.52zM954.88 874.496H69.12a43.52 43.52 0 1 1 0-87.04h885.248c24.064 0 43.52 19.456 43.52 43.52 0.512 24.064-18.944 43.52-43.008 43.52z"
+          fill="#8a8a8a"
+        ></path>
+      </svg>
+    </div>
+  );
+};
+
+const Menu = ({ vertical, onToggle }) => {
+  const router = useRouter();
+  const path = router.pathname;
+
+  return (
+    <div className="navbar-menu-container">
+      {vertical && <div className="navbar-menu-mask" onClick={onToggle}></div>}
+      <ul className={vertical ? "navbar-menu-vertical" : "navbar-menu"}>
+        <li>
+          <Link href="/">
+            <a className={path === "/" ? "active" : null}>首页</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/posts">
+            <a className={path.match(/(^\/post)|(^\/posts)/) ? "active" : null}>
+              秘笈
+            </a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <a>八股</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <a>奇术</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <a>轮子</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <a>鄙人</a>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
 export default function NavBar() {
   const router = useRouter();
   const path = router.pathname;
+
+  const [showMenu, setShowMenu] = useState(false);
 
   // Media Query
   const [isNarrowScreen, setIsNarrorScreen] = useState(false);
@@ -29,63 +93,24 @@ export default function NavBar() {
     };
   }, []);
 
+  useEffect(() => {
+    setShowMenu(false);
+  }, [isNarrowScreen, path]);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-content">
         {isNarrowScreen ? (
-          <div className="navbar-menu">
-            <svg
-              viewBox="0 0 1024 1024"
-              version="1.1"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M954.88 237.056H69.12a43.52 43.52 0 1 1 0-87.04h885.248c24.064 0 43.52 19.456 43.52 43.52s-18.944 43.52-43.008 43.52zM954.88 555.52H69.12a43.52 43.52 0 1 1 0-87.04h885.248c24.064 0 43.52 19.456 43.52 43.52s-18.944 43.52-43.008 43.52zM954.88 874.496H69.12a43.52 43.52 0 1 1 0-87.04h885.248c24.064 0 43.52 19.456 43.52 43.52 0.512 24.064-18.944 43.52-43.008 43.52z"
-                fill="#8a8a8a"
-              ></path>
-            </svg>
-          </div>
+          <MenuButton onToggle={toggleMenu} />
         ) : (
-          <ul>
-            <li>
-              <Link href="/">
-                <a className={path === "/" ? "active" : null}>首页</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/posts">
-                <a
-                  className={
-                    path.match(/(^\/post)|(^\/posts)/) ? "active" : null
-                  }
-                >
-                  秘笈
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/">
-                <a>八股</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/">
-                <a>奇术</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/">
-                <a>轮子</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/">
-                <a>鄙人</a>
-              </Link>
-            </li>
-          </ul>
+          <Menu vertical={isNarrowScreen} />
         )}
       </div>
+      {showMenu && <Menu vertical={isNarrowScreen} onToggle={toggleMenu} />}
     </nav>
   );
 }
